3a7a9fd58f98b65694954a2573e9853746ae8592
su156 <suyadanar.156198@gmail.com>
2020-04-22 22:29:40 +0630

    - Home fragment is implemented.

--- app/src/main/java/com/project/b_mart/fragments/HomeFragment.java
+++ app/src/main/java/com/project/b_mart/fragments/HomeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,7 +16,34 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
+        rootView.findViewById(R.id.girl_fashion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), getString(R.string.girl_fashion), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rootView.findViewById(R.id.boy_fashion).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), getString(R.string.boy_fashion), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rootView.findViewById(R.id.gift_box).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), getString(R.string.gift_box), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        rootView.findViewById(R.id.others).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), getString(R.string.others), Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return rootView;
     }
-
 }
