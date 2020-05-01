ea9dd7dacf37d1a25fba787f59fea2eb29fb9053
su156 <suyadanar.156198@gmail.com>
2020-05-01 08:34:31 +0630

    Show current login user's item list in Profile fragment like facebook.

--- app/src/main/java/com/project/b_mart/fragments/ProfileFragment.java
+++ app/src/main/java/com/project/b_mart/fragments/ProfileFragment.java
@@ -1,18 +1,26 @@
 package com.project.b_mart.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.project.b_mart.R;
+import com.project.b_mart.activities.ItemEditorActivity;
+import com.project.b_mart.adapters.ItemRvAdapter;
+import com.project.b_mart.models.Item;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements ItemRvAdapter.OnListItemClickListener {
+    private ItemRvAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,6 +39,31 @@ public class ProfileFragment extends Fragment {
             tvPhone.setText(user.getPhoneNumber());
         }
 
+        RecyclerView rv = rootView.findViewById(R.id.rv);
+
+        adapter = new ItemRvAdapter(this);
+        rv.setAdapter(adapter);
+        if (getContext() != null) {
+            rv.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        }
+
+        fetchData();
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(int position, Item item) {
+        ItemEditorActivity.setItem(item);
+        startActivity(new Intent(getContext(), ItemEditorActivity.class));
+    }
+
+    @Override
+    public void onLongClick(int position, Item item) {
+        Toast.makeText(getContext(), item.getPrice(), Toast.LENGTH_SHORT).show();
+    }
+
+    private void fetchData() {
+        adapter.setDataSet(ShoppingFragment.generateItems());
     }
 }
